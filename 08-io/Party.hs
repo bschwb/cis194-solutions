b3386a2bf762f62908728f9bc852a77c78a0776e
Bernd Schwarzenbacher <bernd.schwb@yahoo.com>
2015-02-18 13:46:20 +0100

    hw08 finished

---
+++ 08-io/Party.hs
@@ -0,0 +1,54 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Party where
+
+import Data.Monoid
+import Data.Tree
+
+import Employee
+
+main :: IO ()
+main = readFile "company.txt" >>= putStrLn . computeOutput
+
+computeOutput :: String -> String
+computeOutput = formatGL . maxFun . read
+
+formatGL :: GuestList -> String
+formatGL (GL lst fun) = "Total fun: " ++ show fun ++ "\n" ++ unlines employees
+  where employees = map (\(Emp {empName = name}) -> name) lst
+
+instance Monoid GuestList where
+  mempty = GL [] 0
+  mappend (GL al af) (GL bl bf) = GL (al++bl) (af+bf)
+
+-- | Adds Employee to the GuestList and update cached Fun score.
+glCons :: Employee -> GuestList -> GuestList
+glCons emp@(Emp {empFun = ef}) (GL lst gf) = GL (emp:lst) (ef+gf)
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun a b = if a > b then a else b
+
+{-treeFold :: (b -> a -> b) -> b -> Tree a -> b-}
+{-treeFold f init (Node {rootLabel = rl, subForest = sf})-}
+  {-= f (foldl (treeFold f) init sf) rl-}
+
+treeFold :: (a -> [b] -> b) -> b -> Tree a -> b
+treeFold f init (Node {rootLabel = rl, subForest = sf})
+  = f rl (map (treeFold f init) sf)
+
+-- | First part of list is with boss
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel boss bestLists = (maximumS newWithBossL, maximumS newWithoutBossL)
+  where withBossL       = map fst bestLists
+        withoutBossL    = map snd bestLists
+        newWithBossL    = map (glCons boss) withBossL
+        newWithoutBossL = map (glCons boss) withoutBossL
+
+maximumS ::(Monoid a, Ord a) => [a] -> a
+maximumS [] = mempty
+maximumS lst = maximum lst
+
+maxFun :: Tree Employee -> GuestList
+maxFun tree = uncurry max res
+  where res = treeFold nextLevel (mempty, mempty) tree
+
